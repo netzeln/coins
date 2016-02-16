02fce66e1237801c993290d607811eb0f0b635d6
nic netzel and torrence stratton <netzeln@gmail.com>
2016-02-16 14:30:30 -0800

    pass spec test for dimes

--- src/Coin.php
+++ src/Coin.php
@@ -1,7 +1,19 @@
 <?php
 class Coin {
-    function makeChange() {
+
+
+
+    function makeChange($number) {
+        $wallet = array("Quarter" => 0, "Dime"=> 0, "Nickel"=>0, "Penny"=> 0);
+        $Q =(int)($number / 25); echo ($Q);
         
+        $newNumber = ($number - ($Q * 25)); echo ($newNumber);
+        $D = (int)($newNumber / 10); echo($D);
+
+        $wallet["Quarter"]=  $Q;
+        $wallet["Dime"]=  $D;
+var_dump($wallet);
+        return $wallet;
     }
 
 } ?>
--- tests/CoinTest.php
+++ tests/CoinTest.php
@@ -4,17 +4,43 @@
     Class CoinTest extends PHPUnit_Framework_TestCase
     {
 
-        function test_makeChange()
+        // function test_makeChange_penny()
+        // {
+        //     //Arrange
+        //     $test_MakeChange = new Coin;
+        //     $input = 4;
+        //
+        //     //Act
+        //     $result = $test_MakeChange->makeChange($input);
+        //
+        //     //Assert
+        //     $this->assertEquals(array("Quarter" => 0, "Dime"=> 0, "Nickel"=>0, "Penny"=> 4), $result);
+        // }
+
+        function test_makeChange_quarter()
         {
             //Arrange
             $test_MakeChange = new Coin;
-            $input = 1;
+            $input = 50;
 
             //Act
             $result = $test_MakeChange->makeChange($input);
 
             //Assert
-            $this->assertEquals(array("penny"=>1), $result);
+            $this->assertEquals(array("Quarter" => 2, "Dime"=> 0, "Nickel"=>0, "Penny"=> 0), $result);
+        }
+
+        function test_makeChange_dime()
+        {
+            //Arrange
+            $test_MakeChange = new Coin;
+            $input = 20;
+
+            //Act
+            $result = $test_MakeChange->makeChange($input);
+
+            //Assert
+            $this->assertEquals(array("Quarter" => 0, "Dime"=> 2, "Nickel"=>0, "Penny"=> 0), $result);
         }
     }
 
